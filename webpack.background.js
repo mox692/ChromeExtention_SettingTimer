1df6a186d0d83d806302b3ed0abcb09ac677b63b
mox692 <55653825+mox692@users.noreply.github.com>
2020-11-28 21:54:54 +0900

    test runnnig for several webpacconfig

---
+++ webpack.background.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/background/index.ts",
+  
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".json"],
+    },
+  
+    module: {
+      rules: [{ test: /\.ts?$/, loader: "awesome-typescript-loader" }],
+    },
+    
+  mode: "development",
+
+  output: {
+    filename: "bundle.background.js",
+    path: path.join(__dirname, "./extention/dist/background"),
+  },
+
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
+};
